493907cbc2702383b9432927e0525eac6319c340
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-09-09 12:40:17 -0500

    .
    .

---
+++ AlexAntonioGarcíaLandaverde/AlexAntonioGarcíaLandaverde/Controllers/IndexController.cs
@@ -0,0 +1,49 @@
+﻿using AlexAntonioGarcíaLandaverde.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlexAntonioGarcíaLandaverde.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+
+            ClsSimuladorAutomatico Monto = new ClsSimuladorAutomatico() ;
+
+
+            
+            int Multiplo;
+            int Divisible;
+            int v = 5;
+            int c = 5;
+
+            Multiplo = v * c;
+
+            Divisible = v / c;
+
+
+            return View(Monto);
+
+
+
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        public ActionResult MontoRetirar()
+        {
+            return View();
+        }
+    }
+
+
+    
+}
